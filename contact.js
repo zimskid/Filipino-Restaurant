7147a01c44f7acc6be319a7e508366337894368b
zimskid <zimskid@icloud.com>
2021-04-02 08:41:05 -0400

    new update

--- contact.js
+++ contact.js
@@ -1,58 +1,33 @@
-
-var x = document.getElementById("form_sample");
-var createform = document.createElement('form');
-createform.setAttribute("action", ""); 
-createform.setAttribute("method", "post"); 
-x.appendChild(createform);
-
-var heading = document.createElement('h2'); 
-heading.innerHTML = "Contact Form ";
-createform.appendChild(heading);
-
-var line = document.createElement('hr'); 
-createform.appendChild(line);
-
-var linebreak = document.createElement('br');
-createform.appendChild(linebreak);
-
-var namelabel = document.createElement('label'); 
-namelabel.innerHTML = "Your Name : "; 
-createform.appendChild(namelabel);
-
-var inputelement = document.createElement('input'); 
-inputelement.setAttribute("type", "text");
-inputelement.setAttribute("name", "dname");
-createform.appendChild(inputelement);
-
-var linebreak = document.createElement('br');
-createform.appendChild(linebreak);
-
-var emaillabel = document.createElement('label'); 
-emaillabel.innerHTML = "Your Email : ";
-createform.appendChild(emaillabel);
-
-var emailelement = document.createElement('input'); 
-emailelement.setAttribute("type", "text");
-emailelement.setAttribute("name", "demail");
-createform.appendChild(emailelement);
-
-var emailbreak = document.createElement('br');
-createform.appendChild(emailbreak);
-
-var messagelabel = document.createElement('label'); 
-messagelabel.innerHTML = "Your Message : ";
-createform.appendChild(messagelabel);
-
-var texareaelement = document.createElement('textarea');
-texareaelement.setAttribute("name", "dmessage");
-createform.appendChild(texareaelement);
-
-var messagebreak = document.createElement('br');
-createform.appendChild(messagebreak);
-
-var submitelement = document.createElement('input'); 
-submitelement.setAttribute("type", "submit");
-submitelement.setAttribute("name", "dsubmit");
-submitelement.setAttribute("value", "Submit");
-createform.appendChild(submitelement);
-
+function validate(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+  
+    error_message.style.padding = "10px";
+  
+    var text;
+    if(name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 140){
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Email sent to user@email.com");
+    return true;
+  }
